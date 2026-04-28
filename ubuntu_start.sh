6b5483fc7278fd99bbc1b6dd1461028cb352336a
Mohd Abuzar <venomdemon718@gmail.com>
2026-04-28 15:17:58 +0530

    Update ubuntu_start.sh

--- ubuntu_start.sh
+++ ubuntu_start.sh
@@ -5,6 +5,6 @@ echo -e "\e[1;32m         THEVOIDKERNEL UBUNTU RUNNING             \e[0m"
 echo -e "\e[1;34m##################################################\e[0m"
 echo -e "\e[1;35m[thevoidkernel]\e[0m Launching Ubuntu XFCE4..."
 termux-x11 :1 -ac & 
-sleep 2
+udroid login jammy:xfce4
 export DISPLAY=:1
-udroid login jammy:xfce4 -- "export DISPLAY=:1; startxfce4" &
+startxfce4 &
